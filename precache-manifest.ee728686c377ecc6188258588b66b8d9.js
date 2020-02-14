d907567cb680c62f0bf4a8912713c0a754eafe62
David Z. Han <zhaohui.han@gmail.com>
2020-02-13 22:43:38 -0500

    Updates

---
+++ precache-manifest.ee728686c377ecc6188258588b66b8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487ef3c2dbe2bb430f1b4265d68d7012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f789cc34d8df85f2655a",
+    "url": "/static/css/2.adb13d3e.chunk.css"
+  },
+  {
+    "revision": "9d54454392ba919b7b27",
+    "url": "/static/css/main.7130c407.chunk.css"
+  },
+  {
+    "revision": "f789cc34d8df85f2655a",
+    "url": "/static/js/2.71b5c966.chunk.js"
+  },
+  {
+    "revision": "9d54454392ba919b7b27",
+    "url": "/static/js/main.5d3a0d33.chunk.js"
+  },
+  {
+    "revision": "fac5f248ee886af874da",
+    "url": "/static/js/runtime-main.ee1ecdea.js"
+  }
+]);
